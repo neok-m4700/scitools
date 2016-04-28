af9dd42632ee3aec2414e4dc861c7a14a4d65671
neok-m4700 <neok-m4700@users.noreply.github.com>
2016-04-28 18:38:03 +0200

    surface plots vtk examples

--- examples/surf_demo0.py
+++ examples/surf_demo0.py
@@ -1,14 +1,11 @@
-from scitools.std import *
-import time
+from scitools.easyviz import *
+from scitools.easyviz.vtk_new_ import *
 
-xv, yv = ndgrid(linspace(-2,2,41),linspace(-1,1,41))
+xv, yv = ndgrid(linspace(-2, 2, 41), linspace(-1, 1, 41))
 for n, t in enumerate(linspace(0, 4, 21)):
-    values = exp(-0.2*t)*exp(-(2*(xv-t+1)**2 + 10*yv**2))
+    values = exp(-0.2 * t) * exp(-(2 * (xv - t + 1)**2 + 10 * yv**2))
     surf(xv, yv, values, colormap=hot(24), colorbar='on',
          savefig='tmp_%03d.png' % n,
-         zlim=[0,1], caxis=[0, 1])
+         zlim=[0, 1], caxis=[0, 1])
 
 # Note: this demo is very slow in matplotlib (3D)
-input()
-
-
--- examples/surf_demo1.py
+++ examples/surf_demo1.py
@@ -1,53 +1,52 @@
 #!/usr/bin/env python
-
-"""Demonstration on how to use the surf command."""
-
 from scitools.std import *
+from scitools.easyviz import *
+from scitools.easyviz.vtk_new_ import *
 
-xv, yv = ndgrid(linspace(-2,2,21),linspace(-1,1,11))
-values = xv**2*yv-2*yv
+'''Demonstration on how to use the surf command'''
+
+xv, yv = ndgrid(linspace(-2, 2, 21), linspace(-1, 1, 11))
+values = xv**2 * yv - 2 * yv
 setp(show=False)
-subplot(221);  surf(values)
-subplot(222);  surf(xv,yv,values,shading='flat')
-subplot(223);  surf(values,shading='interp')
-subplot(224);  surfc(values,clevels=10)  # add contours beneath the surface
+subplot(221); surf(values)
+subplot(222); surf(xv, yv, values, shading='flat')
+subplot(223); surf(values, shading='interp')
+subplot(224); surfc(values, clevels=10)  # add contours beneath the surface
 setp(show=True)
 show()
 
-#savefig('surf1a.eps')
-#savefig('surf1a.png')
+# savefig('surf1a.eps')
+# savefig('surf1a.png')
 
 figure()
 # Create a surface plot of a sphere:
 n = 32
-theta = pi*linspace(-n,n,n)/n
-theta = reshape(theta, (n,1))
-phi = (pi/2)*linspace(-n,n,n)/n
-phi = reshape(phi, (1,n))
-x = cos(phi)*cos(theta)
-y = cos(phi)*sin(theta)
-z = sin(phi)*ones(shape(theta))
-surf(x,y,z,axis='equal')
+theta = pi * linspace(-n, n, n) / n
+theta = reshape(theta, (n, 1))
+phi = (pi / 2) * linspace(-n, n, n) / n
+phi = reshape(phi, (1, n))
+x = cos(phi) * cos(theta)
+y = cos(phi) * sin(theta)
+z = sin(phi) * ones(shape(theta))
+surf(x, y, z, axis='equal')
 
-#savefig('surf1b.eps')
-#savefig('surf1b.png')
+# savefig('surf1b.eps')
+# savefig('surf1b.png')
 
 figure()
-xv, yv = ndgrid(linspace(-2,2,41),linspace(-2,2,41))
-values = xv*exp(-xv**2-yv**2)
+xv, yv = ndgrid(linspace(-2, 2, 41), linspace(-2, 2, 41))
+values = xv * exp(-xv**2 - yv**2)
 dx, dy = gradient(values)
 c = dx + dy
-surf(xv,yv,values,c,colorbar='on',axis=[-2,2,-2,2,-0.5,0.5])
+surf(xv, yv, values, c, colorbar='on', axis=[-2, 2, -2, 2, -0.5, 0.5])
 
-#savefig('surf1c.eps')
-#savefig('surf1c.png')
+# savefig('surf1c.eps')
+# savefig('surf1c.png')
 
 figure()
-xv, yv = ndgrid(seq(-2.5,2.5,0.15),seq(-5,5,0.15))
-values = 70*yv**2*exp(-xv**2-0.2*yv**2)
-surf(xv,yv,values)
+xv, yv = ndgrid(seq(-2.5, 2.5, 0.15), seq(-5, 5, 0.15))
+values = 70 * yv**2 * exp(-xv**2 - 0.2 * yv**2)
+surf(xv, yv, values)
 
-#savefig('surf1d.eps')
-#savefig('surf1d.png')
-
-input('Press Return key to quit: ')
+# savefig('surf1d.eps')
+# savefig('surf1d.png')
