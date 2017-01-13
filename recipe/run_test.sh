6333f13028a0c262bac218033712c1d14064fb1d
neok-m4700 <neok-m4700@users.noreply.github.com>
2017-01-13 15:29:00 +0100

    update recipe

---
+++ recipe/run_test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# no linkage
+# conda inspect linkages $PKG_NAME
+
+# (set -o posix; set)
+
+# NOTE: we have to extend the PYTHONPATH for the vtk libraries
+# WARNING: we have to change SP_DIR, since it points to the build env and we are now in the test env
+SP_DIR=$(python -c 'import site; print(site.getsitepackages()[0])')
+PYTHONPATH=$SP_DIR/vtk:$PYTHONPATH python tester.py
